409059d3fe2bdc54dae80d16b952365561d3e861
KR-0822 <105578577+KR-0822@users.noreply.github.com>
2022-11-01 10:32:47 +0900

    hmmm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {useState} from "react";
 
 function App() {
+  // const title = ["2월 17일 발행", "남자 코트 추천", "강남 우동맛집" , "파이썬 독학"];
+  const [title, setTitle] = useState(["2월 17일 발행", "남자 코트 추천", "강남 우동맛집" , "파이썬 독학"]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">ReactBlog</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home"> </Nav.Link>
+            <Nav.Link href="#features"> </Nav.Link>
+            <Nav.Link href="#pricing">  </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+      <div className="title"> 
+        <div className="t_head">{title[1]}</div>
+        <div className="t_tail">{title[0]}</div>
+        <hr></hr>
+        <div className="t_head">{title[2]}</div>
+        <div className="t_tail">{title[0]}</div>
+        <hr></hr>
+        <div className="t_head">{title[3]}</div>
+        <div className="t_tail">{title[0]}</div>
+      </div>
+        <button onClick = {()=> {setTitle(["2월 17일 발행", "여자 코트 추천", "강남 우동맛집" , "파이썬 독학"])}}> Click </button>
+    </>
+  ); 
 }
 
 export default App;
